dfe32bf08a400ce76f8140f9b968830cb609da24
RudolfVonStroheim <mgus92896@gmail.com>
2024-01-05 21:28:16 +0300

    end install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 sudo curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install nodejs neovim npm python python-pip
 npm ci coc.nvim
+cp init.vim ~/.config/nvim
 echo "Ready to install coc extensions"
 
